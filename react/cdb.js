4bf810b8c347b3a627bd6270f7197270b027b0d5
AshuuArya <131173626+AshuuArya@users.noreply.github.com>
2024-12-09 21:18:36 +0530

    Add files via upload

---
+++ react/cdb.js
@@ -0,0 +1,7 @@
+const parent=document.getElementById("root");
+const element=React.createElement("h1",(),"This is new heading")
+const root=ReactDOM.createRoot(parent);
+   // root.render(element);
+const h1=React.createElement("h1",(),"shopping list");
+   // root.remder([element,h1])
+const item1=React.createElement("li",(),)
---
+++ react/reactex.js
@@ -0,0 +1,4 @@
+const parent=document.getElementById("root");
+const node=React.createElement("p",{},"this is new paragraph");
+const root=ReactDOM.createRoot(parent);
+root.render(node);
